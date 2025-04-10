7f952642ed6eff89ebc095bae7c534ce35168994
Sarwannandh67 <sarwannandhofficial672007@gmail.com>
2025-04-10 23:16:46 +0530

    Fix case sensitivity issues in UI component imports

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/UI/button"
 
 const Table = React.forwardRef<
   HTMLTableElement,
